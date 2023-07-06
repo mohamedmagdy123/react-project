8a86707d983e75e60eac8b2b2305611e4db4f6db
Mohamed Magdy Mohamed El Shennawy <mohamedmagdy7770@gmail.com>
2023-07-06 13:50:48 +0300

    navbar
    setting the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='p-2 d-flex flex-md-row flex-coloumn justify-content-between'>
+      <div className="left-nav bg-danger flex-md-row flex-coloumn d-flex align-items-center">
+        <h1 className='m-0 pe-3'>Noxe</h1>
+        <ul className='list-unstyled m-0 d-flex '>
+          <li className='px-2'><Link to="home">Home</Link></li>
+          <li className='px-2'><Link to="movies">Movies</Link></li>
+          <li className='px-2'><Link to="tv">Tv</Link></li>
+          <li className='px-2'><Link to="people">People</Link></li>
+        </ul>
+      </div>
+      <div className="right-nav bg-danger flex-md-row flex-coloumn d-flex align-items-center">
+        <div className="social-icons">
+          <i className="fab fa-facebook-f mx-1"></i>
+          <i className="fab fa-twitter mx-1"></i>
+          <i className="fab fa-youtube mx-1"></i>
+          <i className="fab fa-instagram mx-1"></i>
+        </div>
+        <ul className='list-unstyled m-0 d-flex '>
+          <li className='px-2'><Link to="login">Login</Link></li>
+          <li className='px-2'><Link to="/">Register</Link></li>
+          <li className='px-2'><span>Log out</span></li>
+
+        </ul>
+      </div>
+    </nav>
   )
 }
