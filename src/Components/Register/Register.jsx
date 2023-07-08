7c615474a19c48ab6b6aab9f330a09db1914d3e3
Mohamed Magdy Mohamed El Shennawy <mohamedmagdy7770@gmail.com>
2023-07-09 00:04:31 +0300

    finalize register component

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import Joi from 'joi'
 
 
 export default function Register() {
@@ -13,6 +14,7 @@ export default function Register() {
     phone: ""
   })
   const [error, setError] = useState("")
+  const [errorList, setErrorList] = useState([])
   const [isLoading, setisLoading] = useState(false)
 
 
@@ -24,42 +26,93 @@ export default function Register() {
 
 
   async function sendUserDataToApi() {
-    try {
-      let { data } = await axios.post("https://ecommerce.routemisr.com/api/v1/auth/signup", user);
-      setisLoading(false)
-      navigate('/login')
-    } catch (e) {
-      setisLoading(false);
-      setError(e.response.data.errors.param + ': ' + e.response.data.errors.msg);
-    }
-
+    await axios.post("https://ecommerce.routemisr.com/api/v1/auth/signup", user);
+    setisLoading(false)
+    navigate('/login')
   }
 
   function submitUserData(e) {
     setisLoading(true)
     e.preventDefault()
-    sendUserDataToApi()
+    let validation = validateUserData()
+    if (validation === undefined) {
+      setisLoading(false)
+      sendUserDataToApi()
+    } else {
+      setisLoading(false)
+      setErrorList(validation.error.details)
+    }
+  }
+
+
+  function validateUserData() {
+    let schema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().email({ tlds: { allow: ['com', 'net'] } }).required(),
+      password: Joi.string().min(6).max(30).required(),
+      rePassword: Joi.string().valid(Joi.ref('password')).required().empty(),
+      phone: Joi.string().min(11).max(11).required()
+    })
+    setisLoading(false);
+    console.log(schema.validate(user, { abortEarly: false }))
+    return schema.validate(user, { abortEarly: false })
   }
   return (
     <>
-      {error.length > 0 ? <div className='alert alert-danger'>{error}</div> : ''}
-      <form onSubmit={submitUserData} >
+      < form onSubmit={submitUserData} >
         <label htmlFor="name">Name</label>
         <input onChange={getUserData} type="text" className='form-control my-input my-2' name='name' id='name' />
+        {errorList.map((err, index) => {
+          if (err.path[0] === "name") {
+            return (
+              <p key={index} className='text-danger'>{err.message}</p>
+            );
+          }
+        })}
+
         <label htmlFor="email">Email</label>
         <input onChange={getUserData} type="email" className='form-control my-input my-2' name='email' id='email' />
+        {errorList.map((err, index) => {
+          if (err.path[0] === "email") {
+            return (
+              <p key={index} className='text-danger'>{err.message}</p>
+            );
+          }
+        })}
         <label htmlFor="password">Password</label>
         <input onChange={getUserData} type="password" className='form-control my-input my-2' name='password' id='password' />
+        {errorList.map((err, index) => {
+          if (err.path[0] === "password") {
+            return (
+              <p key={index} className='text-danger'>{err.message}</p>
+            );
+          }
+        })}
+
         <label htmlFor="rePassword">re-password</label>
         <input onChange={getUserData} type="password" className='form-control my-input my-2' name='rePassword' id='rePassword' />
+        {errorList.map((err, index) => {
+          if (err.path[0] === "rePassword") {
+            return (
+              <p key={index} className='text-danger'>Passwords don't match</p>
+            );
+          }
+        })}
+
         <label htmlFor="phone">Phone</label>
         <input onChange={getUserData} type="text" className='form-control my-input my-2' name='phone' id='phone' />
-        <button type='submit' className='btn btn-info'>{isLoading == true ? <i className='fas fa-spinner fa-spin'></i> : 'sign Up'}</button>
-
-
-      </form>
+        {errorList.map((err, index) => {
+          if (err.path[0] === "phone") {
+            return (
+              <p key={index} className='text-danger'>{err.message}</p>
+            );
+          }
+        })}
+        <button type='submit' className='btn btn-info'>{isLoading === true ? <i className='fas fa-spinner fa-spin'></i> : 'sign Up'}</button>
+      </form >
     </>
   )
 
 
 }
+
